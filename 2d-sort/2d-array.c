fda21a843dfe475e3ae8f6942bcaf7ae497f81ef
Jared Ellison <jaredellison@gmail.com>
2019-05-18 17:26:29 -0400

    Factor out array filling function

--- 2d-sort/2d-array.c
+++ 2d-sort/2d-array.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-void printArray(int *arr, int len)
+void printArray(int len, int arr[len])
 {
   for (int i = 0; i < len; i++)
   {
@@ -8,21 +8,16 @@ void printArray(int *arr, int len)
   }
 }
 
-int main()
+void fillArray(int lenX, int lenY, int arr[lenX][lenY])
 {
-#define MAX 8
-#define TUPLE_MAX 4
-  int arr[MAX][TUPLE_MAX];
-
-  // Create 2d Array
-  for (int i = 0; i < MAX; i++)
+  for (int i = 0; i < lenX; i++)
   {
-    for (int j = 0; j < TUPLE_MAX; j++)
+    for (int j = 0; j < lenY; j++)
     {
       int val;
       if (j == 0)
       {
-        val = MAX - i;
+        val = lenX - i;
       }
       else
       {
@@ -31,13 +26,26 @@ int main()
       arr[i][j] = val;
     }
   }
+}
 
-  // Print it
+void print2DArray(int lenX, int lenY, int arr[lenX][lenY])
+{
   printf("Elements: \n");
-  for (int i = 0; i < MAX; i++)
+  for (int i = 0; i < lenX; i++)
   {
     printf(" ");
-    printArray(arr[i], TUPLE_MAX);
+    printArray(lenY, arr[i]);
   }
   printf("\n");
+}
+
+int main()
+{
+#define MAX 8
+#define TUPLE_MAX 4
+  int arr[MAX][TUPLE_MAX];
+
+  fillArray(MAX, TUPLE_MAX, arr);
+  // Print it
+  print2DArray(MAX, TUPLE_MAX, arr);
 }
