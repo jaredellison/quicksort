bd24c6683e59a509712c9b455238402e829bcd42
Jared Ellison <jaredellison@gmail.com>
2019-05-18 16:38:37 -0400

    Implement quick sort in place in js

---
+++ js-implementation/quick-sort-in-place.js
@@ -0,0 +1,50 @@
+// Implementation from:
+// https://khan4019.github.io/front-end-Interview-Questions/sort.html
+
+// This approach conserves memory by passing the full array to each function and
+// swaping values in place.
+
+const quickSort = (array, left, right) => {
+  if (left < right) {
+    let pivot = right;
+    let partitionIndex = partition(array, pivot, left, right);
+
+    // sort left
+    quickSort(array, left, partitionIndex - 1);
+    // sort right
+    quickSort(array, partitionIndex + 1, right);
+  }
+  return array;
+};
+
+const partition = (array, pivot, left, right) => {
+  let pivotValue = array[pivot];
+  let partitionIndex = left;
+
+  for (let i = left; i < right; i++) {
+    if (array[i] < pivotValue) {
+      swap(array, i, partitionIndex);
+      partitionIndex++;
+    }
+  }
+  swap(array, right, partitionIndex);
+
+  return partitionIndex;
+};
+
+const swap = (array, a, b) => {
+  let temp = array[a];
+  array[a] = array[b];
+  array[b] = temp;
+  return array;
+};
+
+let unsorted = [5, 2, 3, 4, 1];
+console.log('Expect [1,2,3,4,5]:', quickSort(unsorted, 0, unsorted.length - 1));
+unsorted = [5, 4, 3, 2, 1];
+console.log('Expect [1,2,3,4,5]:', quickSort(unsorted, 0, unsorted.length - 1));
+unsorted = [1, 3, 2, 5, 4];
+console.log('Expect [1,2,3,4,5]:', quickSort(unsorted, 0, unsorted.length - 1));
+
+////////////////////
+//
